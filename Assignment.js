1b60d1ffbc52a938ebbc84dc07396da34dcd1b04
nabsveron <nabsveron13@gmail.com>
2025-09-01 11:24:55 +0300

    assingment

--- Assignment.js
+++ Assignment.js
@@ -2,42 +2,46 @@
 const FullName = document.getElementById('FullName')
 console.log(FullName.value)
 
-FullName.addEventListener('input', (event)=> {
+FullName.addEventListener('input', (event) => {
     console.log(event.target.value)
 })
 // email
 const email = document.getElementById('email')
 console.log(email.value)
-
-email.addEventListener('input', (event)=> {
+email.addEventListener('input', (event) => {
     console.log(event.target.value)
 })
 // username
-
 const username = document.getElementById('username')
 console.log(username.value)
-
-username.addEventListener('input', (event)=> {
+username.addEventListener('input', (event) => {
     console.log(event.target.value)
 })
 // password
 const password = document.getElementById('password')
 console.log(password.value)
-
-password.addEventListener('input', (event)=> {
+password.addEventListener('input', (event) => {
     console.log(event.target.value)
 })
 // confirm password
 const confirmpassword = document.getElementById('confirmpassword')
 console.log(confirmpassword.value)
 
-confirmpassword.addEventListener('input', (event)=> {
+confirmpassword.addEventListener('input', (event) => {
     console.log(event.target.value)
 })
 
 // form
 const myForm = document.getElementById('myForm');
 myForm.addEventListener('submit', (event) => {
-   event.preventDefault();
-   console.log("Form submitted!");
+    event.preventDefault();
+
+    const formData = {
+        name: FullName.value,
+        email: email.value,
+        username: username.value,
+        password: password.value
+    };
+
+    console.log("Form submitted", formData);
 });
